a9e105403bb33ab2f3b8ef362fce1254b9a6311f
fuyutaru <zijian1105@gmail.com>
2025-07-09 12:41:48 +0300

    backup

--- ckanext/zenodo/plugin.py
+++ ckanext/zenodo/plugin.py
@@ -10,9 +10,10 @@ import ckanext.zenodo.helpers as helpers
 # )
 
 
-class ZenodoPlugin(plugins.SingletonPlugin):
+class ZenodoPlugin(plugins.SingletonPlugin, toolkit.DefaultDatasetForm):
     plugins.implements(plugins.IConfigurer)
     plugins.implements(plugins.ITemplateHelpers)
+    plugins.implements(plugins.IDatasetForm, inherit=True)
 
     def update_config(self, config: CKANConfig):
         toolkit.add_template_directory(config, "templates")
@@ -25,3 +26,27 @@ class ZenodoPlugin(plugins.SingletonPlugin):
             'get_zenodo_token': helpers.get_zenodo_token,
             'get_ckan_token': helpers.get_ckan_token
         }
+
+    # def is_fallback(self):
+    #     """
+    #     This plugin is a fallback for the dataset form, so it should be used
+    #     when no other dataset form plugin is available.
+    #     """
+    #     return True
+
+    def setup_template_variables(self, context, data_dict):
+        super(ZenodoPlugin, self).setup_template_variables(context, data_dict)
+        want_doi = data_dict.get("want_doi")
+        # If not present, try to get it from the dataset's extras
+        if not want_doi and "id" in data_dict:
+            # Fetch the dataset using the id
+            pkg = toolkit.get_action("package_show")({}, {"id": data_dict["id"]})
+            for extra in pkg.get("extras", []):
+                if extra["key"] == "want_doi":
+                    want_doi = extra["value"]
+                    break
+        if not want_doi:
+            want_doi = "no"
+        toolkit.c.want_doi = want_doi
+        helpers.setup_template_variables(context, data_dict)
+        return None
